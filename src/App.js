6024a7188276cf029e650950a7a91b40aefa0ee1
vivelaviet <vivelaviet@tamu.edu>
2023-03-23 23:58:01 -0500

    Journal and Working grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+
+import portraits from './import';
 import './App.css';
+import ImageListItem, { imageListItemClasses } from "@mui/material/ImageListItem";
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import journalBG from './assets/backgrounds/journal.png';
+
+const theme = createTheme({
+    breakpoints: {
+      values: {
+        mobile: 0,
+        bigMobile: 350,
+        tablet: 650,
+        desktop: 900
+      }
+    },
+    components:{
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: 'red',
+                    fontFamily:'essay-text',
+                    fontStyle: 'italic',
+                    fontWeight: 700,
+                    textTransform: 'none',
+                }
+            }
+        }
+    }
+  });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <ThemeProvider theme={theme}>
+        <Stack sx={{padding: '2% 1% 0% 0%', float: 'right',}} spacing={2} justifyContent="flex-end">
+            <Button variant="contained">Hunter</Button>
+            <Button variant="contained">Survivor</Button>
+            <Button variant="contained">♂</Button>
+            <Button variant="contained">♀</Button>
+        </Stack>
+
+        <Box sx={{
+          margin: '2% 0% 0% 0%',
+          padding: '2% 5% 2% 5%',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100% 100%",
+          backgroundImage: `url('${journalBG}')`  ,
+          display: "grid",
+          gridTemplateColumns: {
+            mobile: "repeat(2, 1fr)",
+            bigMobile: "repeat(3, 1fr)",
+            tablet: "repeat(7, 1fr)",
+            desktop: "repeat(14, 1fr)"
+          },
+          [`& .${imageListItemClasses.root}`]: {
+            display: "flex",
+            flexDirection: "column"
+          }
+        }}>
+            {portraits.map((item) => (
+                <ImageListItem key={item}>
+                <img
+                    src={`${item}`}
+                    srcSet={`${item}`}
+                    alt={item}
+                    loading="lazy"
+                />
+                </ImageListItem>
+            ))}
+        </Box>
+    
+        </ThemeProvider>
   );
 }
+
+
 
 export default App;
---
+++ src/import.js
@@ -0,0 +1,133 @@
+import Ada from './assets/character-icons/Ada.webp'
+import Aesop from './assets/character-icons/Aesop.webp'
+import Alva from './assets/character-icons/Alva.webp'
+import Andrew from './assets/character-icons/Andrew.webp'
+import Ann from './assets/character-icons/Ann.webp'
+import Anne from './assets/character-icons/Anne.webp'
+import Antonio from './assets/character-icons/Antonio.webp'
+import Bane from './assets/character-icons/Bane.webp'
+import Bonbon from './assets/character-icons/Bonbon.webp'
+import Burke from './assets/character-icons/Burke.webp'
+import Demi from './assets/character-icons/Demi.webp'
+import Edgar from './assets/character-icons/Edgar.webp'
+import Eli from './assets/character-icons/Eli.webp'
+import Emil from './assets/character-icons/Emil.webp'
+import Emily from './assets/character-icons/Emily.webp'
+import Emma from './assets/character-icons/Emma.webp'
+import Fiona from './assets/character-icons/Fiona.webp'
+import Freddy from './assets/character-icons/Freddy.webp'
+import Frederick from './assets/character-icons/Frederick.png'
+import Galatea from './assets/character-icons/Galatea.webp'
+import Ganji from './assets/character-icons/Ganji.webp'
+import Grace from './assets/character-icons/Grace.webp'
+import Hastur from './assets/character-icons/Hastur.webp'
+import Helena from './assets/character-icons/Helena.webp'
+import Ithaqua from './assets/character-icons/Ithaqua.webp'
+import Jack from './assets/character-icons/Jack.webp'
+import Joker from './assets/character-icons/Joker.png'
+import JokerHunter from './assets/character-icons/JokerHunter.webp'
+import Jose from './assets/character-icons/Jose.webp'
+import Joseph from './assets/character-icons/Joseph.webp'
+import Keigan from './assets/character-icons/Keigan.png'
+import Kevin from './assets/character-icons/Kevin.webp'
+import Kreacher from './assets/character-icons/Kreacher.webp'
+import Kurt from './assets/character-icons/Kurt.webp'
+import Leo from './assets/character-icons/Leo.webp'
+import Luca from './assets/character-icons/Luca.webp'
+import Luchino from './assets/character-icons/Luchino.png'
+import LuchinoHunter from './assets/character-icons/LuchinoHunter.webp'
+import LuckyGuy from './assets/character-icons/LuckyGuy.webp'
+import Margie from './assets/character-icons/Margie.webp'
+import Martha from './assets/character-icons/Martha.webp'
+import Mary from './assets/character-icons/Mary.webp'
+import Melly from './assets/character-icons/Melly.webp'
+import Memory from './assets/character-icons/Memory.webp'
+import Michiko from './assets/character-icons/Michiko.webp'
+import Mike from './assets/character-icons/Mike.webp'
+import Murro from './assets/character-icons/Murro.webp'
+import Naib from './assets/character-icons/Naib.webp'
+import Norton from './assets/character-icons/Norton.webp'
+import Orpheus from './assets/character-icons/Orpheus.png'
+import OrpheusHunter from './assets/character-icons/OrpheusHunter.png'
+import Patricia from './assets/character-icons/Patricia.webp'
+import Percy from './assets/character-icons/Percy.webp'
+import Philippe from './assets/character-icons/Philippe.webp'
+import Robbie from './assets/character-icons/Robbie.webp'
+import Servais from './assets/character-icons/Servais.webp'
+import Shiyi from './assets/character-icons/Shiyi.webp'
+import Tracy from './assets/character-icons/Tracy.webp'
+import Vera from './assets/character-icons/Vera.webp'
+import Victor from './assets/character-icons/Victor.webp'
+import Violetta from './assets/character-icons/Violetta.webp'
+import WillBrothers from './assets/character-icons/WillBrothers.webp'
+import WuChang from './assets/character-icons/WuChang.webp'
+import Yidhra from './assets/character-icons/Yidhra.webp'
+
+const portraits =  [
+    Ada,
+    Aesop,
+    Alva ,
+    Andrew,
+    Ann,
+    Anne,
+    Antonio,
+    Bane,
+    Bonbon,
+    Burke,
+    Demi ,
+    Edgar,
+    Eli,
+    Emil ,
+    Emily,
+    Emma,
+    Fiona ,
+    Freddy ,
+    Frederick,
+    Galatea ,
+    Ganji,
+    Grace,
+    Hastur ,
+    Helena ,
+    Ithaqua ,
+    Jack ,
+    Joker ,
+    JokerHunter,
+    Jose,
+    Joseph ,
+    Keigan,
+    Kevin ,
+    Kreacher ,
+    Kurt ,
+    Leo,
+    Luca ,
+    Luchino,
+    LuchinoHunter ,
+    LuckyGuy ,
+    Margie ,
+    Martha,
+    Mary ,
+    Melly,
+    Memory ,
+    Michiko ,
+    Mike ,
+    Murro ,
+    Naib ,
+    Norton,
+    Orpheus,
+    OrpheusHunter,
+    Patricia,
+    Percy,
+    Philippe,
+    Robbie ,
+    Servais ,
+    Shiyi ,
+    Tracy ,
+    Vera ,
+    Victor ,
+    Violetta ,
+    WillBrothers,
+    WuChang ,
+    Yidhra
+];
+
+export default portraits;
